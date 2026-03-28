8e3ce48c3cef5e7fbd7962a8640b9e19e0c7c7c7
Gaurav Kumar <gauravkumar23200200@gmail.com>
2026-03-28 22:49:14 +0530

    Database interlink_v1

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Replace with your actual Supabase details
+<<<<<<< HEAD
 URL = "Subabase URL here"
 KEY = "Supabase Key here"
 URL = "Subabase URL here"
@@ -14,51 +15,10 @@ URL = "Subabase URL here"
 KEY = "Supabase Key here"
 # URL = "" ---- paste URL here-----
 # KEY = "" ----- paste key here-----
+
+URL = ""# ---- paste URL here-----
+KEY = "" #----- paste key here-----
 supabase: Client = create_client(URL, KEY)
-
-# @app.route('/api/login', methods=['POST'])
-# def login():
-#     data = request.json
-#     login_id = data.get('login_id')
-#     password = data.get('password')
-#     role_type = data.get('role')
-
-#     try:
-#         if role_type == 'student':
-#             # Check if Enrollment No AND Password match in the Student table
-#             result = supabase.table("Student").select("S_name") \
-#                 .eq("S_enrollment_no", login_id) \
-#                 .eq("S_password", password) \
-#                 .maybe_single().execute()
-            
-#             if result.data:
-#                 return jsonify({
-#                     "status": "success",
-#                     "redirect": "student-dashboard.html",
-#                     "role": "student",
-#                     "name": result.data['S_name']
-#                 })
-
-#         else: # Faculty
-#             # Check if Email AND Password match in the Faculty table
-#             result = supabase.table("Faculty").select("F_name") \
-#                 .eq("F_email", login_id) \
-#                 .eq("F_password", password) \
-#                 .maybe_single().execute()
-            
-#             if result.data:
-#                 return jsonify({
-#                     "status": "success",
-#                     "redirect": "faculty-dashboard.html",
-#                     "role": "faculty",
-#                     "name": result.data['F_name']
-#                 })
-
-#         # If no match was found in the selected table
-#         return jsonify({"status": "error", "message": "Invalid ID/Email or Password"}), 401
-
-#     except Exception as e:
-#         return jsonify({"status": "error", "message": "Connection error"}), 500
 @app.route('/api/login', methods=['POST'])
 def login():
     data = request.json
